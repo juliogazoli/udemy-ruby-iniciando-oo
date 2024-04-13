e91c351376fa73617378e1d053418bf862dc8b21
Julio Gazoli <julio_gazoli@hotmail.com>
2024-04-13 09:38:57 -0300

    default input and output

---
+++ 02-entrada_e_saida.rb
@@ -0,0 +1,9 @@
+puts "Olá!"
+puts "Digite o seu nome:"
+# v1 = gets
+v1 = gets.chomp # remove \n
+# puts "O seu nome é " + v1
+puts "O seu nome é " + v1.inspect
+puts "Digite sua idade:"
+v2 = gets.chomp
+puts "Sua idade é " + v2
